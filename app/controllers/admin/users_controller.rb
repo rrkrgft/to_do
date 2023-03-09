022d5ad708ee72cf617314b9ba475d73a130b147
kuniyuki isaji <rrkrgft@gmail.com>
2023-03-09 11:18:16 +0900

    テストの実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,15 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー登録しました"
+    else
+      render :new
+    end
+  end
+
   def index
     @user = User.all.includes(:tasks)
   end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -40,5 +40,74 @@ RSpec.describe 'ユーザー機能', type: :system do
         expect(page).to have_content 'ユーザー情報'
       end
     end
+    context 'ログアウトできるか' do
+      it 'ログアウトできるか' do
+        click_on 'ログアウト'
+        expect(page).to have_content 'ログアウトしました'
+        expect(page).to have_content 'Password'
+      end
+    end
+  end
+  describe '管理画面のテスト' do
+    let!(:user1) { FactoryBot.create(:user) }
+    let!(:user_admin) { FactoryBot.create(:user, name: 'admin', email: 'admin@test.com', admin: 'true' ) } 
+    context '管理画面のアクセス' do
+      it '管理者は管理画面にアクセスできる' do
+        visit new_session_path
+        fill_in 'Email', with: 'admin@test.com'
+        fill_in 'Password', with: 'password'
+        click_on 'ログイン'
+        visit admin_users_path
+        expect(page).to have_content 'ユーザー名'
+        expect(page).to have_content 'タスク登録件数'
+      end
+      it '管理権限なしは管理画面にアクセスできない' do
+        visit new_session_path
+        fill_in 'Email', with: 'a@example.com'
+        fill_in 'Password', with: 'password'
+        click_on 'ログイン'
+        visit admin_users_path
+        expect(page).not_to have_content 'ユーザー名'
+        expect(page).to have_content '優先度'
+      end
+    end
+    context '管理ユーザが管理画面の操作を行える' do
+      before do
+        visit new_session_path
+        fill_in 'Email', with: 'admin@test.com'
+        fill_in 'Password', with: 'password'
+        click_on 'ログイン'
+      end
+      it 'ユーザの新規登録ができる' do
+        visit new_admin_user_path
+        fill_in '名前', with: 'test2'
+        fill_in 'メールアドレス', with: 'test2@test.com'
+        fill_in 'パスワード', with: 'password'
+        fill_in '確認用パスワード', with: 'password'
+        click_on '登録'
+        expect(page).to have_content 'ユーザー登録しました'
+        expect(page).to have_content 'test2@test.com'
+      end
+      it 'ユーザーの詳細画面にアクセスできる' do
+        visit admin_user_path(user1)
+        expect(page).to have_content 'a@example.com'
+        expect(page).to have_content '詳細画面'
+      end
+      it 'ユーザーを編集画面から編集できる' do
+        visit edit_admin_user_path(user1)
+        fill_in '名前', with: 'user1'
+        click_on '登録'
+        expect(page).to have_content '編集しました'
+        expect(page).to have_content 'user1'
+      end
+      it 'ユーザーを削除できる' do
+        visit admin_users_path
+        accept_alert do
+          click_on '削除', match: :first
+        end
+        expect(page).to have_content 'ユーザーを削除しました'
+        expect(page).not_to have_content 'a@example.com'
+      end
+    end
   end
 end
