a4d0588d1795cad7f54241c050cd38e3a389a735
kuniyuki isaji <rrkrgft@gmail.com>
2023-03-12 20:21:22 +0900

    テストラベル登録実装

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :label do
-    label { "仕事" }
+    labelname { "仕事" }
   end
 end
--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 RSpec.describe 'ラベル機能', type: :system do
+  let!(:user){ FactoryBot.create(:user)}
+  before do
+    visit new_session_path
+    fill_in "Email", with: "a@example.com"
+    fill_in "Password", with: "password"
+    click_on "ログイン"
+  end
   describe 'ラベル登録のテスト' do
     context 'ラベルの新規登録' do
       it 'ラベルの新規登録ができる' do
@@ -10,8 +17,8 @@ RSpec.describe 'ラベル機能', type: :system do
         expect(page).to have_content "家事"
       end
     end
+    let!(:label){ FactoryBot.create(:label)}
     before do
-      let!(:label){ FactoryBot.create(:label)}
       visit labels_path
     end
     context 'ラベルの編集ができる' do
